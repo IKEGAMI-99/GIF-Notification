d40dbc8c0ca85ae79a5f93495fa330876034894e
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-23 19:59:28 +0900

    Restore compileSdk 37 for current Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ikegami99.gifnotification"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.ikegami99.gifnotification"
