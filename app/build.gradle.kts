06c4210822a5622c31c01e9173edd5fd65a61adf
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-23 19:55:12 +0900

    Use stable Android 36 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ikegami99.gifnotification"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ikegami99.gifnotification"
